ae46d0477ad46094a81f98d4d6b06ab855634bc8
civaca <30809584+civaca@users.noreply.github.com>
2017-08-10 22:35:41 -0500

    files
    adding files of the wabe page

---
+++ functions.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  
+  $("ul").addClass("list-group");
+  $("li").addClass("list-group-item ");
+     $("h6").addClass("list-group-item-heading lead");
+      $(".years").addClass("list-group-item-text");
+
+   });
